d4b85f16b7ad7ee8233ea52a6377b46eeeeabe07
star1120eto <etoile.letoile@gmail.com>
2024-06-15 09:19:19 +0900

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+  public static void main(String[] args) { 
+    int age;
+    age = 31;
+    System.out.println("生年月日を足すと");
+    System.out.println(1990+7+16);
+    System.out.println("それに年齢を足すと");
+    System.out.println(1990+7+16+age);
+  }
+}
