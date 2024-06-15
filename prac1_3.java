9eb784c4710adc037721a1b78d36cf27e1de3c8f
star1120eto <etoile.letoile@gmail.com>
2024-06-16 07:31:56 +0900

    add:part1 practice

---
+++ prac1_3.java
@@ -0,0 +1,9 @@
+public class prac1_3 {
+    public static void main(String[] args) {
+        boolean is = true;
+        char dash = '駆';
+        final double PI = 3.1419;
+        final long PI_LONG=314159265853878L;
+        String message = "私の攻撃！敵に15ポイントのダメージ。";
+    }
+}
---
+++ square.java
@@ -0,0 +1,10 @@
+public class square {
+    public static void main(String[] args) {
+        int vertical = 3; // 縦
+        int horizontal = 5; // 横
+        int area = vertical * horizontal; // 面積
+
+        System.out.println("縦幅" + vertical + "横幅" + horizontal + "の長方形の面積は、" + area);
+
+    }
+}
